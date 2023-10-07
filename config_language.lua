634d1279a815b1bff456358cf13b6148ddcee165
FafaCeek <87832404+Ceek11@users.noreply.github.com>
2023-10-07 21:52:32 +0200

    Add files via upload

---
+++ config_language.lua
@@ -0,0 +1,16 @@
+
+TranslationIllegal = {
+    VentePnj = {
+        ["ActiverVenteDrogue"] = "Vous avez activé la vente de drogue !",
+        ["DesactiverVenteDrogue"] = "Vous avez désactivé la vente de drogue !",
+        ["AppuyerPourVendre"] = "Appuyez sur ~INPUT_CONTEXT~ pour vendre votre drogue.",
+        ["DroguesVendre"] = "↓ ~b~Drogues que vous pouvez vendre~s~ ↓",
+        ["AttenteReflexion"] = "Attendez un instant, je réfléchis.",
+        ["PasInteresse"] = "Je ne suis pas intéressé !",
+        ["Merci"] = "Merci mon gars !",
+        ["RienAVendre"] = "~r~Tu n'as rien à vendre",
+        ["PasDeTelephone"] = "Vous n'avez pas de téléphone de bicrave sur vous",
+        ["VenteEffectuee"] = "Vous venez de vendre x%s %s pour %s$",
+        ["NotEnoughPolice"] = "Il n'y a pas assez de policiers",
+    },    
+}
